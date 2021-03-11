ac624087eb1c5c7ea6f7c12e5fc2d70189274dd0
Me <mergorgec@gmail.com>
2021-03-11 21:30:14 +0100

    add rendering lists key prop

--- react-fundamentals/src/exercise/07.js
+++ react-fundamentals/src/exercise/07.js
@@ -1,5 +1,5 @@
 // Rendering Lists
-// http://localhost:3000/isolated/exercise/07.js
+// http://localhost:3000/isolated/final/07.js
 
 import * as React from 'react'
 
@@ -31,8 +31,7 @@ function App() {
       </button>
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
